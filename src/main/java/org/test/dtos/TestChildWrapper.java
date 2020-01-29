ec5c6cc9a86b4da88518607c03e36cf13cc10055
Simeon Mugisha <cmeon@users.noreply.github.com>
2020-01-29 12:22:02 +0300

    Fix json creation

--- src/main/java/org/test/dtos/TestChildWrapper.java
+++ src/main/java/org/test/dtos/TestChildWrapper.java
@@ -6,12 +6,18 @@ import java.util.Set;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonCreator;
 
 
 @AllArgsConstructor
 @Data
 @NoArgsConstructor
 public class TestChildWrapper implements Serializable {
+
+  @JsonCreator
+  public TestChildWrapper(String childWrapper) {
+    super();
+  }
 
   private static final long serialVersionUID = 1L;
 
